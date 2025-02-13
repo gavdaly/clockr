688767eb3471f10914cda8a84da8bbc6c2c3dff0
Gavin Daly <gav.d@me.com>
2025-02-12 23:52:20 -0500

    remove cfg_if

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use cfg_if::cfg_if;
-
 pub mod app;
 pub mod components;
 pub mod database;
@@ -11,18 +9,10 @@ pub mod service;
 pub mod user;
 pub mod utils;
 
-cfg_if! { if #[cfg(feature = "hydrate")] {
-    use leptos::prelude::*;
-    use wasm_bindgen::prelude::wasm_bindgen;
-    use leptos::mount::mount_to_body;
-    use crate::app::*;
-
-    #[wasm_bindgen]
-    pub fn hydrate() {
-        // initializes logging using the `log` crate
-        _ = console_log::init_with_level(log::Level::Debug);
-        console_error_panic_hook::set_once();
-
-        mount_to_body(App);
-    }
-}}
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    use crate::app::App;
+    console_error_panic_hook::set_once();
+    leptos::mount::hydrate_body(App);
+}
