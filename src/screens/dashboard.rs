ad6331c25e45179fb78b4a2500f48383c41e814a
Gavin Daly <gav.d@me.com>
2025-02-16 02:13:58 -0500

    feat(dashboard): implement time entry management

--- src/screens/dashboard.rs
+++ src/screens/dashboard.rs
@@ -1,24 +1,91 @@
-use leptos::form::ActionForm;
+use chrono::Timelike;
 use leptos::prelude::*;
+use leptos::{form::ActionForm, tachys::dom::window};
 
 use crate::{functions::user::CheckIn, models::user::UserDisplay};
+
+#[derive(Clone, Debug)]
+struct TimeEntry {
+    id: String,
+    time: String,
+    timestamp: u64,
+}
 
 /// Renders the home page of your application.
 #[component]
 pub fn Dashboard() -> impl IntoView {
     let (user, set_user) = signal::<Option<UserDisplay>>(None);
-    let today = Memo::new(move |_| 7326);
+
+    let timestamp = |time| {
+        chrono::NaiveTime::parse_from_str(time, "%H:%M")
+            .unwrap()
+            .num_seconds_from_midnight() as u64
+    };
+
+    let (times, set_times) = signal(vec![
+        TimeEntry {
+            id: "1".to_string(),
+            time: "8:03 AM".to_string(),
+            timestamp: timestamp("08:03"),
+        },
+        TimeEntry {
+            id: "2".to_string(),
+            time: "12:00 PM".to_string(),
+            timestamp: timestamp("12:00"),
+        },
+        TimeEntry {
+            id: "3".to_string(),
+            time: "3:00 PM".to_string(),
+            timestamp: timestamp("15:00"),
+        },
+    ]);
+
+    let delete_item = move |id: String| {
+        set_times.update(|t| {
+            t.retain(|entry| entry.id != id);
+        });
+    };
+
+    let today = Memo::new(move |_| {
+        let t = times.get();
+        let len = t.len();
+        if len == 0 {
+            return 0;
+        }
+        let prev: u64 = t
+            .chunks(2)
+            .map(|te| {
+                if te.len() == 2 {
+                    te[1].timestamp - te[0].timestamp
+                } else {
+                    0
+                }
+            })
+            .sum();
+
+        if len % 2 != 0 {
+            let _last_entry = t.last().unwrap();
+        }
+        prev
+    });
     let week = Memo::new(move |_| 23554);
     view! {
         <section class="stack">
             <ClkIn />
             <div id="check-ins" class="card wide">
                 <h2>"Check Ins"</h2>
-                <p> add time </p>
-                <ul>
-                    <li><time datetime="">"8:00 AM"</time></li>
-                    <li><time datetime="">"12:00 PM"</time></li>
-                </ul>
+                <p> "add time +"</p>
+                <ul class="slide-list">
+                            {move || times.get().into_iter().map(|entry| {
+
+                                view! {
+                                    <li>
+                                        <time datetime="">{entry.time}</time>
+                                        <div class="delete-indicator">Delete</div>
+                                    </li>
+                                }
+                            }).collect::<Vec<_>>()}
+                        </ul>
             </div>
             <div class="card small">
                 <h2>"Today"</h2>
@@ -69,6 +136,5 @@ fn ShowError(error: Memo<Option<ServerFnError>>) -> impl IntoView {
         <div class="error">
             {move || error.get().as_ref().map(|e| view! {<p>{e.to_string()}</p>})}
         </div>
-
     }
 }
