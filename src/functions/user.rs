e06a9ad900508e9ae78aad6af43799bcfbffe76f
Gavin Daly <gav.d@me.com>
2025-02-18 22:51:12 -0500

    Sessions working

--- src/functions/user.rs
+++ src/functions/user.rs
@@ -43,32 +43,38 @@ pub async fn get_current_user() -> Result<Option<UserToday>, ServerFnError> {
 #[server]
 #[tracing::instrument]
 async fn check_in() -> Result<UserToday, ServerFnError> {
-    // use crate::models::sessions::{close_session, get_open_session, new_session};
-    use axum_session::SessionAnySession;
-    use leptos::prelude::server_fn::error::*;
+    use crate::models::sessions::{close_session, get_open_session, new_session};
+    use axum::extract::Extension;
+    use axum_session::Session;
+    use axum_session_sqlx::SessionPgPool;
+    use leptos_axum::extract;
     use uuid::Uuid;
 
-    let Some(session) = use_context::<SessionAnySession>() else {
-        error!("Cound not load session context");
-        return Err(ServerFnError::<NoCustomError>::ServerError(
-            "Session missing.".into(),
-        ));
+    let session: Extension<Session<SessionPgPool>> = extract().await?;
+    info!("Session: {:?}", session);
+
+    let Some(id) = session.get("id") else {
+        trace!("User not Authenticated, Could not find ID: {:?}", session);
+        return Err(ServerFnError::ServerError("User not authenticated".into()));
     };
 
-    let Some(id) = session.get::<Uuid>("id") else {
-        info!("User not Authenticated");
-        leptos_axum::redirect("/");
-        return Err(ServerFnError::<NoCustomError>::ServerError(
-            "Error getting Session!".into(),
-        ));
+    let Ok(user) = UserToday::get(id).await else {
+        error!("Could not find User for session");
+        return Err(ServerFnError::ServerError("Could Not Find User.".into()));
     };
 
-    let Ok(user) = crate::models::user::UserToday::get(id).await else {
-        error!("Could not find User");
-        return Err(ServerFnError::<NoCustomError>::ServerError(
-            "Could Not Find User.".into(),
-        ));
+    match get_open_session(&user.id).await {
+        Ok(user_session) => {
+            close_session(&user_session.id).await?;
+        }
+        _ => {
+            new_session(&user.id).await?;
+        }
     };
 
+    let Ok(user) = UserToday::get(id).await else {
+        error!("Could not find User for session");
+        return Err(ServerFnError::ServerError("Could Not Find User.".into()));
+    };
     Ok(user)
 }
